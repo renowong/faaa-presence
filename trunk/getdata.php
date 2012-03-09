2caf39c96fe9004475c174d0b66d2994d51331a5
renowong@gmail.com <renowong@gmail.com@a2773c53-5124-6692-d304-f31d093a51d6>
2012-03-09 18:48:03 +0000

    loaddata ok

---
+++ trunk/getdata.php
@@ -0,0 +1,32 @@
+<?php
+include_once('config.php');
+//header('Content-Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+echo '<response>';
+echo query();
+echo '</response>';
+
+
+function query() {
+	$year = $_POST['year'];
+	$month = $_POST['month'];
+	$agentid = $_POST['agentid'];;
+
+	$mysqli = new mysqli(DBSERVER, DBUSER, DBPWD, DB);
+	////set the query
+	$query = sprintf("SELECT * FROM `presence` WHERE `year`='%s' AND `month`='%s' AND `agentid`='%s'",$year,$month,$agentid);
+	$result = $mysqli->query($query);
+
+		if ($result){
+                    while($row = $result->fetch_object()){
+                    $idtag = $row->type."_".$row->agentid."_".$row->day;
+                    $xml .= "<data idtag='$idtag'>".$row->value."</data>";            
+                    }
+		} else {
+		    echo $mysqli->error;
+		}
+		$mysqli->close();
+        return $xml;
+}
+
+?>
--- trunk/presence.php
+++ trunk/presence.php
@@ -1,82 +1,6 @@
 <?php
 include_once('config.php');
-if($_GET["load"]){
-    $selectedmonth = $_GET["month"];
-    $selectedyear = $_GET["year"]; 
-}else{
-    $selectedmonth = date("m");
-    $selectedyear = date("Y"); 
-}
-
-$ar_agents = getagents();
-$months = buildmonths($selectedmonth);
-$years = buildyears($selectedyear);
-
-function buildmonths($s){
-    $ar_month = array(1=>'Janvier',2=>'F&eacute;vrier',3=>'Mars',4=>'Avril',5=>'Mai',6=>'Juin',7=>'Juillet',8=>'Ao&ucirc;t',9=>'Septembre',10=>'Octobre',11=>'Novembre',12=>'D&eacute;cembre');
-    for ($i=1;$i<=12;$i++){
-        if($s==$i){$select=" selected";}else{$select="";}
-        $output .= "<option value=\"".$i."\"$select>".$ar_month[$i]."</option>";
-    }
-    //$output = print_r($ar_month);
-    return $output;
-}
-
-function buildyears($s){
-    $ar_year = array(2012=>'2012',2013=>'2013',2014=>'2014');
-    for ($i=2012;$i<=2014;$i++){
-        if($s==$i){$select=" selected";}else{$select="";}
-        $output .= "<option value=\"".$i."\"$select>".$ar_year[$i]."</option>";
-    }
-    //$output = print_r($ar_month);
-    return $output;
-}
-
-function getagents($svc){
-        $ar_agent = array();
-    	$mysqli = new mysqli(DBSERVER, DBUSER, DBPWD, DB);
-	////set the query
-	$query = sprintf("SELECT * FROM `agents` WHERE `service`='%s'","INF");
-	$result = $mysqli->query($query);
-        while($row = $result->fetch_object()){
-        array_push($ar_agent,array($row->agentid,$row->nom,$row->prenom));            
-        }
-        $mysqli->close();
-        
-        return $ar_agent;
-}
-
-function buildcol($input,$id,$month,$year){
-    $max = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-    for ($i=1; $i<=$max; $i++)
-    {
-        switch ($input){
-            case "num":
-                print "<td>$i</td>";
-            break;
-            case "hd":
-                print "<td>";
-                if(!WE($i,$month,$year)) print "<input type=\"text\" size=\"2\" maxlength=\"5\" name=\"hd_".$id."_".$i."\" id=\"hd_".$id."_".$i."\" onBlur=\"update(this);\" />";
-                print "</td>";
-            break;
-            case "ha":
-                print "<td>";
-                if(!WE($i,$month,$year)) print "<input type=\"text\" size=\"2\" maxlength=\"5\" name=\"ha_".$id."_".$i."\" id=\"ha_".$id."_".$i."\" onBlur=\"update(this);\" />";
-                print "</td>";
-            break;
-        }
-    }
-}
-
-function WE($day,$month,$year){
-    $lday = date("l", mktime(0, 0, 0, $month, $day, $year));
-    if($lday=="Saturday" || $lday=="Sunday"){
-        return true;
-    }else{
-        return false;
-    }
-}
-
+include_once('presence_top.php');
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -88,7 +12,34 @@ function WE($day,$month,$year){
         <script type="text/javascript">
         $(document).ready(function () {
         
+        //load data
+        var ids = $("#agentsids").val();
+        var ar_ids = ids.split("|");
+        for(i=0;i<ar_ids.length;i++){
+            getdata($("#editmonth").val(),$("#edityear").val(),ar_ids[i]);
+        }
+        
         });
+        
+        
+        
+        function getdata(month,year,agentid){
+            $.post("getdata.php", {month:month,year:year,agentid:agentid},
+                function(response) {
+                loaddata(response);
+                //alert(response);
+                });
+            
+        }
+        
+        function loaddata(xml){
+            //alert("extracting xml");
+            $(xml).find("data").each(function(){
+                var tag = $(this).attr("idtag");
+                var value = $(this).text();
+                $("#"+tag).val(value);
+             });
+        }
         
         function loaddate(){
             var month = $("#slt_month").val();
@@ -109,7 +60,7 @@ function WE($day,$month,$year){
             $.post("update.php", {year:edityear,month:editmonth,day:editday,type:type,value:fvalue,agentid:agentid},
 			function(response) {
 			//readresponse(response);
-			alert(response);
+			//alert(response);
 			});
             
         }
@@ -117,6 +68,7 @@ function WE($day,$month,$year){
 </head><body>
 <input type="hidden" id="editmonth" value="<? print $selectedmonth; ?>"/>
 <input type="hidden" id="edityear" value="<? print $selectedyear; ?>"/>
+<input type="hidden" id="agentsids" value="<? print $agentsids; ?>"/>
 <button onclick="window.location='presence.php';">Mois en cours</button>
 <p>Mois : <select id="slt_month">
     <? print $months ?>
