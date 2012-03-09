dfcc7b73a42e1669e8b6f5b47230209f15252b44
renowong@gmail.com <renowong@gmail.com@a2773c53-5124-6692-d304-f31d093a51d6>
2012-03-09 00:34:07 +0000

    loading and adaptation of grid, ok

--- trunk/presence.php
+++ trunk/presence.php
@@ -1,6 +1,36 @@
 <?php
 include_once('config.php');
+if($_GET["load"]){
+    $selectedmonth = $_GET["month"];
+    $selectedyear = $_GET["year"]; 
+}else{
+    $selectedmonth = date("m");
+    $selectedyear = date("Y"); 
+}
+
 $ar_agents = getagents();
+$months = buildmonths($selectedmonth);
+$years = buildyears($selectedyear);
+
+function buildmonths($s){
+    $ar_month = array(1=>'Janvier',2=>'F&eacute;vrier',3=>'Mars',4=>'Avril',5=>'Mai',6=>'Juin',7=>'Juillet',8=>'Ao&ucirc;t',9=>'Septembre',10=>'Octobre',11=>'Novembre',12=>'D&eacute;cembre');
+    for ($i=1;$i<=12;$i++){
+        if($s==$i){$select=" selected";}else{$select="";}
+        $output .= "<option value=\"".$i."\"$select>".$ar_month[$i]."</option>";
+    }
+    //$output = print_r($ar_month);
+    return $output;
+}
+
+function buildyears($s){
+    $ar_year = array(2012=>'2012',2013=>'2013',2014=>'2014');
+    for ($i=2012;$i<=2014;$i++){
+        if($s==$i){$select=" selected";}else{$select="";}
+        $output .= "<option value=\"".$i."\"$select>".$ar_year[$i]."</option>";
+    }
+    //$output = print_r($ar_month);
+    return $output;
+}
 
 function getagents($svc){
         $ar_agent = array();
@@ -16,47 +46,79 @@ function getagents($svc){
         return $ar_agent;
 }
 
-function buildcol($input,$id){
-    $thisyear = date("Y");
-    $thismonth = date("m");
-    $max = cal_days_in_month(CAL_GREGORIAN, $thismonth, $thisyear);
+function buildcol($input,$id,$month,$year){
+    $max = cal_days_in_month(CAL_GREGORIAN, $month, $year);
     for ($i=1; $i<=$max; $i++)
     {
         switch ($input){
             case "num":
-                print "<td style=\"vertical-align: top;\">$i</td>";
+                print "<td>$i</td>";
             break;
             case "hd":
-                print "<td style=\"vertical-align: top;\">".
-                "<input type=\"text\" size=\"5\" maxlength=\"5\" id=\"hd_".$id."_".$i."\" />".
-                "</td>";
+                print "<td>";
+                if(!WE($i,$month,$year)) print "<input type=\"text\" size=\"2\" maxlength=\"5\" id=\"hd_".$id."_".$i."\" />";
+                print "</td>";
             break;
             case "ha":
-                print "<td style=\"vertical-align: top;\">".
-                "<input type=\"text\" size=\"5\" maxlength=\"5\" id=\"ha_".$id."_".$i."\" />".
-                "</td>";
+                print "<td>";
+                if(!WE($i,$month,$year)) print "<input type=\"text\" size=\"2\" maxlength=\"5\" id=\"ha_".$id."_".$i."\" />";
+                print "</td>";
             break;
         }
     }
 }
 
-function checkWE($day,$month,$year){
-    
+function WE($day,$month,$year){
+    $lday = date("l", mktime(0, 0, 0, $month, $day, $year));
+    if($lday=="Saturday" || $lday=="Sunday"){
+        return true;
+    }else{
+        return false;
+    }
 }
+
+
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html><head>
-<title></title></head><body>
-<table style="text-align: left; width: 100%;" border="1" cellpadding="2" cellspacing="2">
+<title></title>
+    <style type="text/css">@import url("css/main.css");</style>
+        <!-- jquery -->
+        <script type="application/x-javascript" src="js/jquery-1.7.min.js"></script>
+        <script type="text/javascript">
+        $(document).ready(function () {
+        
+        });
+        
+        function loaddate(){
+            var month = $("#slt_month").val();
+            var year = $("#slt_annee").val();
+            window.location = "presence.php?load=true&month="+month+"&year="+year;
+        }
+    </script>
+</head><body>
+<input type="hidden" id="editmonth" value="<? print $selectedmonth; ?>"/>
+<input type="hidden" id="edityear" value="<? print $selectedyear; ?>"/>
+<button onclick="window.location='presence.php';">Mois en cours</button>
+<p>Mois : <select id="slt_month">
+    <? print $months ?>
+</select>
+Ann&eacute;e :
+<select id="slt_annee">
+    <? print $years ?>
+</select>
+<button onclick="loaddate();">Charger</button>
+</p>
+<table>
   <tbody>
     <tr>
-      <td style="vertical-align: top;">Agent
+      <td>Agent
       </td>
-      <td style="vertical-align: top;">HA/HD
+      <td>HA/HD
       </td>
-        <? print buildcol("num") ?>
-      <td style="vertical-align: top;">Total
+        <? print buildcol("num","",$selectedmonth,$selectedyear) ?>
+      <td>Total
       </td>
     </tr>
     <?
@@ -64,7 +126,7 @@ function checkWE($day,$month,$year){
         print "<tr>";
         print "<td style=\"vertical-align: top;\">".$ar_agent[1]."</td>";
         print "<td style=\"vertical-align: top;\">HA</td>";
-        print buildcol("ha",$ar_agent[0]);
+        print buildcol("ha",$ar_agent[0],$selectedmonth,$selectedyear);
         print "<td colspan=\"1\" rowspan=\"2\" style=\"vertical-align: top;\">";
         print "<span id=\"total_".$ar_agent[0]."\" />";
         print "</td></tr>";
@@ -72,7 +134,7 @@ function checkWE($day,$month,$year){
         print "<td style=\"vertical-align: top;\">".$ar_agent[2]."</td>";
         print "<td style=\"vertical-align: top;\">HD</td>";
         print "</td>";
-        print buildcol("hd",$ar_agent[0]);
+        print buildcol("hd",$ar_agent[0],$selectedmonth,$selectedyear);
         print "</tr>";
     }
     ?>
