bf841c9f64771900b212fa643e4bcdb549d56848
renowong@gmail.com <renowong@gmail.com@a2773c53-5124-6692-d304-f31d093a51d6>
2012-03-16 20:55:47 +0000

    extract excel xml oksvn add *

--- excel/days.php
+++ excel/days.php
@@ -9,14 +9,14 @@ for ($i=1;$i<=31;$i++){
 $days .="<Cell ss:MergeDown=\"1\" ss:StyleID=\"ce49\"><Data ss:Type=\"String\">TOTAL Heure de travail</Data></Cell><Cell ss:MergeDown=\"1\" ss:StyleID=\"ce57\"><Data ss:Type=\"String\">BULLETIN
 PAIE</Data></Cell><Cell ss:MergeDown=\"1\" ss:StyleID=\"ce64\"><Data ss:Type=\"String\">PRESENCE</Data></Cell><Cell ss:Index=\"236\" ss:StyleID=\"ce5\"/></Row>";
 
-$days .= "<Row><Cell ss:Index=\"3\" ss:StyleID=\"ce26\"><Data ss:Type=\"String\">1</Data></Cell>";
+$days .= "<Row ss:Height=\"25\"><Cell ss:Index=\"3\" ss:StyleID=\"ce26\"><Data ss:Type=\"Number\">1</Data></Cell>";
 
 for ($i=2;$i<=31;$i++){
-    $days .= "<Cell ss:StyleID=\"ce26\"><Data ss:Type=\"String\">$i</Data></Cell>";
+    $days .= "<Cell ss:StyleID=\"ce26\"><Data ss:Type=\"Number\">$i</Data></Cell>";
 }
 
 $days .="</Row>";
 
-$days .="<Row ss:AutoFitHeight=\"0\" ss:Height=\"28.4904\"><Cell ss:MergeAcross=\"33\" ss:StyleID=\"ce6\"><Data ss:Type=\"String\">C E L L U L E     I N F O R M A T I Q U E</Data></Cell></Row>";
+$days .="<Row ss:AutoFitHeight=\"0\" ss:Height=\"28.4904\"><Cell ss:MergeAcross=\"35\" ss:StyleID=\"ce6\"><Data ss:Type=\"String\">C E L L U L E     I N F O R M A T I Q U E</Data></Cell></Row>";
 
 ?>
